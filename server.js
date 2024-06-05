91db41bd3db52656650b6924fc75465aae011765
shahidx0x <dev.imshahid@gmail.com>
2024-06-06 01:59:10 +0600

    (feat/add) : added basic routes ,controller and models

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect("mongodb://localhost:27017/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-  });
+connectDB();
 
-// Middleware
-app.use(express.json());
-
-// Routes
-app.get("/", async (req, res) => {
-  res.status(200).json({ message: "api-is-ok" });
-});
-
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
